8b8838b886f1b94144ea61ecc5efcf5e63d15130
Demion-L <padovds@ukr.net>
2023-01-11 10:47:37 +0200

    Castom hook added

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function useDebounce(value: string, delay: number = 300): string {
+  const [debounced, setDebounced] = React.useState(value);
+
+  React.useEffect(() => {
+    const handler = setTimeout(() => setDebounced(value), delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+}
