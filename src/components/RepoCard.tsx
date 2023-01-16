bd4eec9248e51d4084261a2a48df4ce4b7d8e04b
Demion-L <padovds@ukr.net>
2023-01-16 14:26:27 +0200

    logic adding and removing favs added

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { IRepo } from "../models/models";
 import { useActions } from "../hooks/actions";
+import { useAppSelector } from "../hooks/redux";
 
 export default function RepoCard({ repo }: { repo: IRepo }) {
-  const { addFavourite } = useActions();
+  const { addFavourite, removeFavourite } = useActions();
+  const { favourites } = useAppSelector((state) => state.github);
 
-  const addToFafourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const [isFav, setIsFav] = React.useState(favourites.includes(repo.html_url));
+
+  const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     addFavourite(repo.html_url);
+    setIsFav(true);
+  };
+
+  const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    removeFavourite(repo.html_url);
+    setIsFav(false);
   };
 
   return (
@@ -20,9 +31,21 @@ export default function RepoCard({ repo }: { repo: IRepo }) {
         </p>
         <p className='text-sm font-thin'>{repo?.description}</p>
 
-        <button
-          className='py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all'
-          onClick={addToFafourite}></button>
+        {!isFav && (
+          <button
+            className='py-2 px-4 mr-2 bg-yellow-400 rounded hover:shadow-md transition-all'
+            onClick={addToFavourite}>
+            add to favourite
+          </button>
+        )}
+
+        {isFav && (
+          <button
+            className='py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all'
+            onClick={removeFromFavourite}>
+            Remove from favourite
+          </button>
+        )}
       </a>
     </div>
   );
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { useAppSelector } from "../hooks/redux";
 
 export default function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  const { favourites } = useAppSelector((state) => state.github);
+  if (favourites.length === 0) {
+    return <p className='text-center'>No items.</p>;
+  }
+
+  return (
+    <ul>
+      {favourites.map((f) => (
+        <li key={f}>
+          <a href={f} target='_blank'>
+            {f}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
