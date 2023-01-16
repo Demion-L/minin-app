58d29b1820edcaa8abf53bd565b7d60a961d13c2
Demion-L <padovds@ukr.net>
2023-01-16 13:06:16 +0200

    add favourite logic added

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { IRepo } from "../models/models";
+import { useActions } from "../hooks/actions";
 
 export default function RepoCard({ repo }: { repo: IRepo }) {
+  const { addFavourite } = useActions();
+
+  const addToFafourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    addFavourite(repo.html_url);
+  };
+
   return (
     <div className='border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all'>
       <a href={repo.html_url} target='_blank'>
@@ -11,6 +19,10 @@ export default function RepoCard({ repo }: { repo: IRepo }) {
           Watchers: <span className='font-bold'>{repo.watchers}</span>
         </p>
         <p className='text-sm font-thin'>{repo?.description}</p>
+
+        <button
+          className='py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all'
+          onClick={addToFafourite}></button>
       </a>
     </div>
   );
